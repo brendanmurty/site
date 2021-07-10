2cc88234e67780c2788bb25cc30e25b09b5f2889
Brendan Murty <b@murty.io>
2021-07-10 15:54:13 +1000

    Core: Improve setup process for Unix-based systems

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,8 +7,19 @@ mkdir -p public
 # Setup an initial ENV file if it doesn't already exist
 cp -n .env.example .env
 
-# Allow the local Deno binary to run on this machine
-chmod +x ./scripts/bin/linux/deno
-chmod +x ./scripts/bin/macos/deno
+# Setup Git LFS and allow the local Deno binary to run on this machine
+if [ "$(uname -s)" == "Linux" ]
+then
+  sudo apt install -y git git-lfs
+  git-lfs install
+  chmod +x ./scripts/bin/linux/deno
+elif [ "$(uname -s)" == "Darwin" ]
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install git-lfs
+  chmod +x ./scripts/bin/macos/deno
+else
+  git lfs install
+fi
 
 echo "Please edit '.env' to suit your environment."
