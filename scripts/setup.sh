408b86c205a059b5618425e145c546b07ff9c5c0
Brendan Murty <b@murty.io>
2021-07-10 15:46:56 +1000

    Core: Fix execution status of Deno binaries

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,8 @@ mkdir -p public
 # Setup an initial ENV file if it doesn't already exist
 cp -n .env.example .env
 
+# Allow the local Deno binary to run on this machine
+chmod +x ./scripts/bin/linux/deno
+chmod +x ./scripts/bin/macos/deno
+
 echo "Please edit '.env' to suit your environment."
