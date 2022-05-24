3d57f2bbcf55d2b710c1fdf196d10b571796b56d
Brendan Murty <b@murty.io>
2022-05-24 12:08:12 +1000

    Core: Remove obsolete Git LFS setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,19 +10,4 @@ mkdir -p public
 # Setup an initial ENV file if it doesn't already exist
 cp -n .env.example .env
 
-# Setup Git LFS and allow the local Deno binary to run on this machine
-if [ "$(uname -s)" == "Linux" ]
-then
-  sudo apt install -y git git-lfs
-  git-lfs install
-  chmod +x ./bin/linux/deno
-elif [ "$(uname -s)" == "Darwin" ]
-then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  brew install git-lfs
-  chmod +x ./bin/macos/deno
-else
-  git lfs install
-fi
-
-echo "Please edit '.env' to suit your environment."
+echo "Please edit '.env' to suit this environment."
