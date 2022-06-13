99b2ffac9de9bc900bbc55df28bc8f5e1b58575e
Brendan Murty <b@murty.io>
2022-06-13 15:39:27 +1000

    Tests: Progress for JSON items test

--- tests/json-feed.test.ts
+++ tests/json-feed.test.ts
@@ -1,20 +1,29 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { assertEquals, assertNotEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
 import { existsSync } from "https://deno.land/std@0.143.0/fs/mod.ts";
 import { isJSON } from "https://deno.land/x/is_json@v1.0.2/mod.ts";
-import "https://deno.land/x/dotenv@v3.2.0/load.ts";
-
-// import { validate } from "https://deno.land/x/schema_validator@v0.0.3/mod.ts";
-// import { YamlData, JsonFeedData, JsonFeedAuthor, JsonFeedItem } from "./types.ts";
-
+import { posix } from "https://deno.land/std@0.140.0/path/mod.ts";
+import { YamlData, JsonFeedData, JsonFeedAuthor, JsonFeedItem } from "../src/types.ts";
 
 Deno.test("src/json-feed.ts", async(test) => {
   const postsJsonFile: string = Deno.env.get("JSON_FEED_FILE_OUTPUT") || "";
+  const postsDirectory: string = Deno.env.get("JSON_FEED_POSTS_DIR") || "";
 
   await test.step({
     name: "required var in env file is set (JSON_FEED_FILE_OUTPUT)",
     fn: () => {
       assertNotEquals(
         postsJsonFile,
+        ""
+      );
+    }
+  });
+
+  await test.step({
+    name: "required var in env file is set (JSON_FEED_POSTS_DIR)",
+    fn: () => {
+      assertNotEquals(
+        postsDirectory,
         ""
       );
     }
@@ -84,12 +93,37 @@ Deno.test("src/json-feed.ts", async(test) => {
     }
   });
 
+  // await test.step({
+  //   name: "post JSON contains the right number of items",
+  //   fn: async () => {
+  //     const postsJsonContent: string = await Deno.readTextFile(postsJsonFile);
+  //     const postsJsonObject = postsJsonContent as unknown as JsonFeedData;
+  //     const countItemsInJSONFile = Object.keys(postsJsonObject.items).length;
+
+  //     const postsDirectoryAbsolute = posix.join(Deno.cwd(), postsDirectory);
+
+  //     let countValidPostFiles = 0;
+  //     for await (const item of Deno.readDir(postsDirectoryAbsolute)) {
+  //       if (item.isFile && item.name != "index.md" && item.name.slice(-3) == ".md") {
+  //         countValidPostFiles++;
+  //       }
+  //     }
+      
+  //     console.log('valid files', countValidPostFiles);
+  //     console.log('items in JSON', countValidPostFiles);
+
+  //     assertEquals(
+  //       countValidPostFiles,
+  //       countItemsInJSONFile
+  //     );
+  //   }
+  // });
+
   // TODO: Add test - each item in the "items" array in the JSON file matches the "JsonFeedItem" type definition
 
   // TODO: Add test - the main properties of the JSON file matches the "JsonFeedData" type definition
 
   // TODO: Add test - the "author" property of the JSON file matches the "JsonFeedAuthor" type definition
 
-  // TODO: Add test - the "items" array in the JSON file contents contains the right number of items it it
   
 });
