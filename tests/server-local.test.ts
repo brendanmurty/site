92c88b72549db50c7c079ac23cdf3932500ee75f
Brendan Murty <b@murty.io>
2022-06-13 10:58:00 +1000

    Tests: Comment out unfinished test

--- tests/server-local.test.ts
+++ tests/server-local.test.ts
@@ -1,39 +1,39 @@
-import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
+// import { assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
 
-Deno.test("src/server-local.ts", async(test) => {
-  await test.step({
-    name: "run script",
-    fn: async () => {
-      // TODO: Update this to run in the background while the tests are running
-      const script_run = Deno.run({
-        cmd: [
-          "deno",
-          "run",
-          "--allow-read",
-          "--allow-net",
-          "--allow-write",
-          "--allow-env",
-          "src/server-local.ts"
-        ],
-        stdout: "piped",
-        stderr: "piped",
-      });
+// Deno.test("src/server-local.ts", async(test) => {
+//   await test.step({
+//     name: "run script",
+//     fn: async () => {
+//       // TODO: Update this to run in the background while the tests are running
+//       const script_run = Deno.run({
+//         cmd: [
+//           "deno",
+//           "run",
+//           "--allow-read",
+//           "--allow-net",
+//           "--allow-write",
+//           "--allow-env",
+//           "src/server-local.ts"
+//         ],
+//         stdout: "piped",
+//         stderr: "piped",
+//       });
 
-      const { code } = await script_run.status();
+//       const { code } = await script_run.status();
 
-      script_run.stdout.close();
-      script_run.stderr.close();
-      script_run.close();
+//       script_run.stdout.close();
+//       script_run.stderr.close();
+//       script_run.close();
 
-      assertEquals(code, 0);
-    }
+//       assertEquals(code, 0);
+//     }
 
-  });
+//   });
 
-  // TODO: Add test - server has started
+//   // TODO: Add test - server has started
 
-  // TODO: Add test - a ping to the localhost domain returns a valid status code
+//   // TODO: Add test - a ping to the localhost domain returns a valid status code
   
-  // TODO: Kill the background "script_run" process when all tests have been completed
+//   // TODO: Kill the background "script_run" process when all tests have been completed
 
-});
+// });
