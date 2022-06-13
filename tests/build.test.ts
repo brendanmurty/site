128486de2eb7d49a4bdcb6b0a771abdac84487d9
Brendan Murty <b@murty.io>
2022-06-13 21:22:45 +1000

    Tests: Add build script tests

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -3,7 +3,7 @@ import { assertEquals, assertNotEquals } from "https://deno.land/std@0.143.0/tes
 Deno.test("bin/build", async(test) => {
 
   await test.step({
-    name: "public directory exists and is not empty",
+    name: "non-empty file 'public/index.html' was found",
     fn: async () => {
       try {
         const publicIndexFileContents: string = await Deno.readTextFile("public/index.html");
@@ -18,10 +18,83 @@ Deno.test("bin/build", async(test) => {
           ""
         );
       }
-      
     }
   });
 
-  // TODO: Add test - public directory has the required sub-directories and files
+  await test.step({
+    name: "non-empty file 'public/css/styles.min.css' was found",
+    fn: async () => {
+      try {
+        const publicIndexFileContents: string = await Deno.readTextFile("public/css/styles.min.css");
+
+        assertNotEquals(
+          publicIndexFileContents,
+          ""
+        );
+      } catch (_) {
+        assertEquals(
+          "File not found",
+          ""
+        );
+      }
+    }
+  });
+
+  await test.step({
+    name: "non-empty file 'public/brendan/index.html' was found",
+    fn: async () => {
+      try {
+        const publicIndexFileContents: string = await Deno.readTextFile("public/brendan/index.html");
+
+        assertNotEquals(
+          publicIndexFileContents,
+          ""
+        );
+      } catch (_) {
+        assertEquals(
+          "File not found",
+          ""
+        );
+      }
+    }
+  });
+
+  await test.step({
+    name: "non-empty file 'public/brendan/posts.json' was found",
+    fn: async () => {
+      try {
+        const publicIndexFileContents: string = await Deno.readTextFile("public/brendan/posts.json");
+
+        assertNotEquals(
+          publicIndexFileContents,
+          ""
+        );
+      } catch (_) {
+        assertEquals(
+          "File not found",
+          ""
+        );
+      }
+    }
+  });
+
+  await test.step({
+    name: "non-empty file 'public/images/brendan/brendan-murty-2021.jpeg' was found",
+    fn: async () => {
+      try {
+        const publicIndexFileContents: string = await Deno.readTextFile("public/images/brendan/brendan-murty-2021.jpeg");
+
+        assertNotEquals(
+          publicIndexFileContents,
+          ""
+        );
+      } catch (_) {
+        assertEquals(
+          "File not found",
+          ""
+        );
+      }
+    }
+  });
 
 });
