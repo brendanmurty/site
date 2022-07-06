921f8123b4bba112aed0eacab44c522cd06df5c7
Brendan Murty <brendan@murty.au>
2022-07-06 20:47:44 +1000

    Core: Setup shortcut domain redirects for Deno Deploy site

--- src/server-deno-deploy.ts
+++ src/server-deno-deploy.ts
@@ -4,9 +4,40 @@ const app = new Application();
 
 app.use(async (ctx, next) => {
   try {
-    // TODO: Setup redirects to handle shortcut redirects like b.murty.au ---> murty.au/brendan
-    // const request_domain: string = ctx.request.headers.get('host')?.split(':')[0]);
+    // Redirect the user when a shortcut domain is requested
+    const request_domain: string = ctx.request.headers.get('host')?.split(':')[0] || '';
+    switch (request_domain) {
+      case 'b.murty.io':
+      case 'brendan.murty.io':
+      case 'b.murty.au':
+      case 'brendan.murty.au':
+      case 'brendanmurty.com':
+        ctx.response.redirect(`https://murty.au/brendan`);
+        break;
+      case 'f.murty.io':
+      case 'freya.murty.io':
+      case 'f.murty.au':
+      case 'freya.murty.au':
+      case 'freyamurty.com':
+        ctx.response.redirect(`https://murty.au/freya`);
+        break;
+      case 'i.murty.io':
+      case 'isla.murty.io':
+      case 'i.murty.au':
+      case 'isla.murty.au':
+      case 'islamurty.com':
+        ctx.response.redirect(`https://murty.au/isla`);
+        break;
+      case 'l.murty.io':
+      case 'luca.murty.io':
+      case 'l.murty.au':
+      case 'luca.murty.au':
+      case 'lucamurty.com':
+        ctx.response.redirect(`https://murty.au/luca`);
+        break;
+    }
 
+    // Serve the request from the static site directory
     await ctx.send({
       root: `${Deno.cwd()}/public`,
       index: "index.html",
@@ -16,15 +47,5 @@ app.use(async (ctx, next) => {
   }
 });
 
-const router = new Router();
-
-// The /api/time endpoint returns the current time in ISO format.
-router.get("/api/time", (ctx) => {
-  ctx.response.body = { time: new Date().toISOString() };
-});
-
-// After creating the router, we can add it to the app.
-app.use(router.routes());
-app.use(router.allowedMethods());
-
+// Listen for requests on port 8000
 await app.listen({ port: 8000 });
