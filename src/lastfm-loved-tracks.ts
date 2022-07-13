b640b1424c2bcd82028adc473dc5101186b2bef1
Brendan Murty <brendan@murty.au>
2022-07-13 14:37:24 +1000

    Core: Simplify Last.fm track image method

--- src/lastfm-loved-tracks.ts
+++ src/lastfm-loved-tracks.ts
@@ -1,14 +1,15 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
-export async function LastFmLovedTracks(): Promise<{ artist: string; track: string; image: string; url: string; }[]> {
+export async function LastFmLovedTracks(): Promise<{ artist: string; track: string; url: string; }[]> {
   const lastFmApiKey: string = Deno.env.get("LASTFM_API_KEY") || "";
+  const lastFmApiUrl = "http://ws.audioscrobbler.com/2.0/";
   const lastFmUsername = "brendanmurty";
 
   if (!lastFmApiKey) {
     throw new Error("LastFmLovedTracks - Required ENV variable (LASTFM_API_KEY) is not set.");
   }
 
-  const apiRequestUrl: string = 'http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=' + lastFmUsername + '&limit=5&api_key=' + lastFmApiKey + '&format=json';
+  const apiRequestUrl: string = lastFmApiUrl + '?method=user.getlovedtracks&user=' + lastFmUsername + '&limit=5&api_key=' + lastFmApiKey + '&format=json';
 
   const apiResponse = await fetch(apiRequestUrl);
   const apiResponseBody = await apiResponse.json();
@@ -19,12 +20,9 @@ export async function LastFmLovedTracks(): Promise<{ artist: string; track: stri
 
   let lovedTracks = [];
   for (const track of apiResponseBody.lovedtracks.track) {
-    // TODO: Get the correct image URL here - https://www.last.fm/api/show/track.getInfo
-
     lovedTracks.push({
       "artist": track.artist.name,
       "track": track.name,
-      "image": track.image[2]["#text"],
       "url": track.url,
     });
   }
