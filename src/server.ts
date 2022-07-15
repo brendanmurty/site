ce3b633b6d9c93f9fe54a5d28ec6ba121eea5b06
Brendan Murty <brendan@murty.au>
2022-07-15 13:02:44 +1000

    Server: Add redirect for UpcomingTasks domain

--- src/server.ts
+++ src/server.ts
@@ -12,34 +12,37 @@ app.use(async (ctx, next) => {
       case 'b.murty.au':
       case 'brendan.murty.au':
       case 'brendanmurty.com':
-        ctx.response.redirect(`https://murty.au/brendan`);
+        ctx.response.redirect('https://murty.au/brendan');
+        break;
+      case 'upcomingtasks.com':
+        ctx.response.redirect('https://murty.au/brendan/posts/farewell-upcomingtasks/');
         break;
       case 'f.murty.io':
       case 'freya.murty.io':
       case 'f.murty.au':
       case 'freya.murty.au':
       case 'freyamurty.com':
-        ctx.response.redirect(`https://murty.au/freya`);
+        ctx.response.redirect('https://murty.au/freya');
         break;
       case 'i.murty.io':
       case 'isla.murty.io':
       case 'i.murty.au':
       case 'isla.murty.au':
       case 'islamurty.com':
-        ctx.response.redirect(`https://murty.au/isla`);
+        ctx.response.redirect('https://murty.au/isla');
         break;
       case 'l.murty.io':
       case 'luca.murty.io':
       case 'l.murty.au':
       case 'luca.murty.au':
       case 'lucamurty.com':
-        ctx.response.redirect(`https://murty.au/luca`);
+        ctx.response.redirect('https://murty.au/luca');
         break;
     }
 
     // Serve the request from the static site directory
     await ctx.send({
-      root: `${Deno.cwd()}/public`,
+      root: '${Deno.cwd()}/public',
       index: "index.html",
     });
   } catch (error) {
