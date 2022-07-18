fb122225897338a94648301860abb4db35ed7d9f
Brendan Murty <brendan@murty.au>
2022-07-18 13:39:36 +1000

    DevOps: Split out site data processing to a new script instead

--- _config.js
+++ _config.js
@@ -8,27 +8,6 @@
 
 import lume from "https://deno.land/x/lume@v1.9.0/mod.ts";
 import date from "https://deno.land/x/lume@v1.9.0/plugins/date.ts";
-import { json2yaml } from "https://deno.land/x/json2yaml@v1.0.1/mod.ts";
-import { PostsLatest } from "./src/posts-latest.ts";
-import { LastFmLovedTracks } from "./src/lastfm-loved-tracks.ts";
-
-// Convert the contents of ".env" in to YAML format and save it as "building/_data/site.yml".
-// This allows for the Nunjucks templates to access these variables like this: {{ site.PIRSCH_ANALYTICS_SITE_CODE }}
-const envFileContent = Deno.readTextFileSync("./.env");
-let ymlFileContent = envFileContent.replaceAll("=", ": ");
-
-// Add the latest post properties to the site data
-let latestPost = await PostsLatest();
-latestPost = '{ "BLOG_POSTS_LATEST": ' + JSON.stringify(latestPost) + '}';
-ymlFileContent += "\r" + json2yaml(latestPost);
-
-// Add the Last.fm loved tracks to the site data
-let lastfmLovedTracks = await LastFmLovedTracks();
-lastfmLovedTracks = '{ "LASTFM_LOVED_TRACKS": ' + JSON.stringify(lastfmLovedTracks) + '}';
-ymlFileContent += "\r" + json2yaml(lastfmLovedTracks);
-
-// Save the YAML file so it can be accessed by the page templates
-Deno.writeTextFileSync("./building/_data/site.yml", ymlFileContent);
 
 // Build the site using Lume
 const site = lume({
---
+++ src/site-data.ts
@@ -0,0 +1,21 @@
+import { json2yaml } from "https://deno.land/x/json2yaml@v1.0.1/mod.ts";
+import { PostsLatest } from "./posts-latest.ts";
+import { LastFmLovedTracks } from "./lastfm-loved-tracks.ts";
+
+// Convert the contents of ".env" in to YAML format and save it as "building/_data/site.yml".
+// This allows for the Nunjucks templates to access these variables like this: {{ site.PIRSCH_ANALYTICS_SITE_CODE }}
+const EnvFileContent = Deno.readTextFileSync("./.env");
+let YmlFileContent = EnvFileContent.replaceAll("=", ": ");
+
+// Add the latest post properties to the site data
+const LatestPost = await PostsLatest();
+const DataLatestPost = '{ "BLOG_POSTS_LATEST": ' + JSON.stringify(LatestPost) + '}';
+YmlFileContent += "\r" + json2yaml(DataLatestPost);
+
+// Add the Last.fm loved tracks to the site data
+const LastfmLovedTracks = await LastFmLovedTracks();
+const DataLastfmLovedTracks = '{ "LASTFM_LOVED_TRACKS": ' + JSON.stringify(LastfmLovedTracks) + '}';
+YmlFileContent += "\r" + json2yaml(DataLastfmLovedTracks);
+
+// Save the YAML file so it can be accessed by the page templates
+Deno.writeTextFileSync("./building/_data/site.yml", YmlFileContent);
