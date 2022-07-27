e037369f2e62966e9c3c2174356b648a8bb87426
Brendan Murty <brendan@murty.au>
2022-07-28 09:42:48 +1000

    DevOps: Improve error handling in server script

--- src/server.ts
+++ src/server.ts
@@ -2,56 +2,59 @@ import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 const app = new Application();
 
-app.use(async (ctx, next) => {
+app.use(async (context, next) => {
   try {
     // Redirect the user when a shortcut domain is requested
-    const request_domain: string = ctx.request.headers.get('host')?.split(':')[0] || '';
+    const request_domain: string = context.request.headers.get('host')?.split(':')[0] || '';
     switch (request_domain) {
       case 'b.murty.io':
       case 'brendan.murty.io':
       case 'b.murty.au':
       case 'brendan.murty.au':
       case 'brendanmurty.com':
-        ctx.response.redirect('https://murty.au/brendan');
+        context.response.redirect('https://murty.au/brendan');
         break;
       case 'upcomingtasks.com':
-        ctx.response.redirect('https://murty.au/brendan/posts/farewell-upcomingtasks/');
+        context.response.redirect('https://murty.au/brendan/posts/farewell-upcomingtasks/');
         break;
       case 'f.murty.io':
       case 'freya.murty.io':
       case 'f.murty.au':
       case 'freya.murty.au':
       case 'freyamurty.com':
-        ctx.response.redirect('https://murty.au/freya');
+        context.response.redirect('https://murty.au/freya');
         break;
       case 'i.murty.io':
       case 'isla.murty.io':
       case 'i.murty.au':
       case 'isla.murty.au':
       case 'islamurty.com':
-        ctx.response.redirect('https://murty.au/isla');
+        context.response.redirect('https://murty.au/isla');
         break;
       case 'l.murty.io':
       case 'luca.murty.io':
       case 'l.murty.au':
       case 'luca.murty.au':
       case 'lucamurty.com':
-        ctx.response.redirect('https://murty.au/luca');
+        context.response.redirect('https://murty.au/luca');
         break;
     }
 
     // Serve the request from the static site directory
-    await ctx.send({
+    await context.send({
       root: Deno.cwd() + '/public',
       index: 'index.html'
     });
   } catch (error) {
-    if (error) {
-      console.error(error);
-    }
-
-    next();
+    console.log('Error: ' + error.message);
   }
+  
+  next();
+});
+
+// Catch errors and simplify the log message for them
+app.addEventListener('error', (error) => {
+  console.log('Error: ' + error.message);
 });
 
 // Listen for requests on port 8000
