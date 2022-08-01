c7f13475bfbe54652220a9822ff5b7313c58e9f4
Brendan Murty <brendan@murty.au>
2022-08-01 14:52:34 +1000

    DevOps: Log errors then redirect to avoid browser default error pages on 404s

--- src/server.ts
+++ src/server.ts
@@ -1,58 +1,61 @@
-import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import { Application, isHttpError } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
 const app = new Application();
 
-app.use(async (_context, next) => {
+app.use(async (context) => {
   try {
-    await next();
+    // Redirect the user when a shortcut domain is requested
+    const request_domain: string = context.request.headers.get('host')?.split(':')[0] || '';
+    switch (request_domain) {
+      case 'b.murty.io':
+      case 'brendan.murty.io':
+      case 'b.murty.au':
+      case 'brendan.murty.au':
+      case 'brendanmurty.com':
+        context.response.redirect('https://murty.au/brendan');
+        break;
+      case 'upcomingtasks.com':
+        context.response.redirect('https://murty.au/brendan/posts/farewell-upcomingtasks/');
+        break;
+      case 'f.murty.io':
+      case 'freya.murty.io':
+      case 'f.murty.au':
+      case 'freya.murty.au':
+      case 'freyamurty.com':
+        context.response.redirect('https://murty.au/freya');
+        break;
+      case 'i.murty.io':
+      case 'isla.murty.io':
+      case 'i.murty.au':
+      case 'isla.murty.au':
+      case 'islamurty.com':
+        context.response.redirect('https://murty.au/isla');
+        break;
+      case 'l.murty.io':
+      case 'luca.murty.io':
+      case 'l.murty.au':
+      case 'luca.murty.au':
+      case 'lucamurty.com':
+        context.response.redirect('https://murty.au/luca');
+        break;
+    }
+
+    // Serve the request from the static site directory
+    await context.send({
+      root: Deno.cwd() + '/public',
+      index: 'index.html'
+    });
   } catch (error) {
     // Catch errors and simplify the log message for them
-    console.log('Error: ' + error.message);
-  }
-});
+    if (isHttpError(error)) {
+      console.log('Error: ' + error.message + ' (' + context.request.url.href + ')');
+    } else {
+      console.log('Error: ' + error.message);
+    }
 
-app.use(async (context) => {
-  // Redirect the user when a shortcut domain is requested
-  const request_domain: string = context.request.headers.get('host')?.split(':')[0] || '';
-  switch (request_domain) {
-    case 'b.murty.io':
-    case 'brendan.murty.io':
-    case 'b.murty.au':
-    case 'brendan.murty.au':
-    case 'brendanmurty.com':
-      context.response.redirect('https://murty.au/brendan');
-      break;
-    case 'upcomingtasks.com':
-      context.response.redirect('https://murty.au/brendan/posts/farewell-upcomingtasks/');
-      break;
-    case 'f.murty.io':
-    case 'freya.murty.io':
-    case 'f.murty.au':
-    case 'freya.murty.au':
-    case 'freyamurty.com':
-      context.response.redirect('https://murty.au/freya');
-      break;
-    case 'i.murty.io':
-    case 'isla.murty.io':
-    case 'i.murty.au':
-    case 'isla.murty.au':
-    case 'islamurty.com':
-      context.response.redirect('https://murty.au/isla');
-      break;
-    case 'l.murty.io':
-    case 'luca.murty.io':
-    case 'l.murty.au':
-    case 'luca.murty.au':
-    case 'lucamurty.com':
-      context.response.redirect('https://murty.au/luca');
-      break;
+    // Redirect to the site listing page
+    context.response.redirect('https://murty.au/');
   }
-
-  // Serve the request from the static site directory
-  await context.send({
-    root: Deno.cwd() + '/public',
-    index: 'index.html'
-  });
 });
 
 // Listen for requests on port 8000
