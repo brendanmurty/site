b94207f2b729596d6e3ff8004b0135ab2b55c284
Brendan Murty <brendan@murty.au>
2023-01-24 14:53:55 +1100

    Photos: Add comments

--- src/photo-data.ts
+++ src/photo-data.ts
@@ -1,8 +1,11 @@
 import { create } from "../vendor/deno-exif/mod.ts";
 
 export function GetExifDataFromPhoto(photoDirectory: string, photoFile: string): Record<string,string> {
+  // Get the EXIF data from the image
   const parser = create(Deno.readFileSync(photoDirectory + '/' + photoFile));
   const result = parser.parse();
+
+  // Format the data in to human-friendly content assuming photos may not have values for these properties 
   const photoDevice: string = (result.tags.Make && result.tags.Model) ? (result.tags?.Make + ' ' + result.tags?.Model) : '';
   const photoDimensions: string = result.imageSize.width + ' x ' + result.imageSize.height;
   const photoLens: string = (result.tags.LensMake && result.tags.LensModel) ? (result.tags.LensMake + ' ' + result.tags.LensModel) : '';
