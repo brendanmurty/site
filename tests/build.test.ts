39f91f913aea264f733f0d590e40a75ec14a759e
Brendan Murty <brendan@boulevardglobal.com>
2023-04-17 14:07:29 +1000

    Brendan: Update new avatar image file names, update references

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -62,11 +62,11 @@ Deno.test("bin/build", async (test) => {
   });
 
   await test.step({
-    name: "non-empty file 'public/images/brendan/brendan-murty-2023.jpg' was found",
+    name: "non-empty file 'public/images/brendan/brendan-luca-2023_200.jpg' was found",
     fn: async () => {
       try {
         const publicIndexFileContents: string = await Deno.readTextFile(
-          "public/images/brendan/brendan-murty-2023.jpg"
+          "public/images/brendan/brendan-luca-2023_200.jpg"
         );
 
         assertNotEquals(publicIndexFileContents, "");
