2db848f2884baf178f8d20f9c58fc4bc4978663a
Brendan Murty <brendan@boulevardglobal.com>
2023-12-22 10:15:48 +1100

    Server: Add page redirect, domain redirect cleanup

--- src/server.ts
+++ src/server.ts
@@ -6,42 +6,53 @@ import {
 const app = new Application();
 
 app.use(async (context) => {
-  try {
-    // Redirects based on the domain requested by the visitor
-    const request_domain: string =
-      context.request.headers.get("host")?.split(":")[0] || "";
-    switch (request_domain) {
-      case "murty.io":
-      case "www.murty.au":
-      case "www.murty.io":
-        context.response.redirect("https://murty.au/");
-        break;
-      case "b.murty.io":
-      case "brendan.murty.io":
-      case "b.murty.au":
-      case "brendan.murty.au":
-        context.response.redirect("https://murty.au/brendan");
-        break;
-      case "f.murty.io":
-      case "freya.murty.io":
-      case "f.murty.au":
-      case "freya.murty.au":
-        context.response.redirect("https://murty.au/freya");
-        break;
-      case "i.murty.io":
-      case "isla.murty.io":
-      case "i.murty.au":
-      case "isla.murty.au":
-        context.response.redirect("https://murty.au/isla");
-        break;
-      case "l.murty.io":
-      case "luca.murty.io":
-      case "l.murty.au":
-      case "luca.murty.au":
-        context.response.redirect("https://murty.au/luca");
-        break;
-    }
+  // Page redirects
+  if (
+    context.request.url.pathname ===
+    "/brendan/posts/20230306_lead-software-engineer-at-boulevard/"
+  ) {
+    context.response.redirect(
+      "/brendan/posts/20230306_lead-software-engineer-at-liquidise"
+    );
+    return true;
+  }
 
+  // Domain redirects
+  const request_domain: string =
+    context.request.headers.get("host")?.split(":")[0] || "";
+  switch (request_domain) {
+    case "murty.io":
+    case "www.murty.au":
+    case "www.murty.io":
+      context.response.redirect("https://murty.au/");
+      break;
+    case "b.murty.io":
+    case "brendan.murty.io":
+    case "b.murty.au":
+    case "brendan.murty.au":
+      context.response.redirect("https://murty.au/brendan");
+      break;
+    case "f.murty.io":
+    case "freya.murty.io":
+    case "f.murty.au":
+    case "freya.murty.au":
+      context.response.redirect("https://murty.au/freya");
+      break;
+    case "i.murty.io":
+    case "isla.murty.io":
+    case "i.murty.au":
+    case "isla.murty.au":
+      context.response.redirect("https://murty.au/isla");
+      break;
+    case "l.murty.io":
+    case "luca.murty.io":
+    case "l.murty.au":
+    case "luca.murty.au":
+      context.response.redirect("https://murty.au/luca");
+      break;
+  }
+
+  try {
     // Serve remaining valid requests from the static site directory
     await context.send({
       root: Deno.cwd() + "/public",
