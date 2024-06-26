cb436d98e7b79e65ee5a014a8be1a6a4f7567a5d
Brendan Murty <brendan@murty.au>
2024-06-26 23:03:14 +1000

    Server: Remove obsolete domain redirects

--- src/server.ts
+++ src/server.ts
@@ -18,42 +18,6 @@ app.use(async (context) => {
     return true;
   }
 
-  // Domain redirects
-  const request_domain: string =
-    context.request.headers.get("host")?.split(":")[0] || "";
-  switch (request_domain) {
-    case "murty.email":
-    case "murty.io":
-    case "www.murty.au":
-    case "www.murty.io":
-      context.response.redirect("https://murty.au/");
-      break;
-    case "b.murty.io":
-    case "brendan.murty.io":
-    case "b.murty.au":
-    case "brendan.murty.au":
-      context.response.redirect("https://murty.au/brendan");
-      break;
-    case "f.murty.io":
-    case "freya.murty.io":
-    case "f.murty.au":
-    case "freya.murty.au":
-      context.response.redirect("https://murty.au/freya");
-      break;
-    case "i.murty.io":
-    case "isla.murty.io":
-    case "i.murty.au":
-    case "isla.murty.au":
-      context.response.redirect("https://murty.au/isla");
-      break;
-    case "l.murty.io":
-    case "luca.murty.io":
-    case "l.murty.au":
-    case "luca.murty.au":
-      context.response.redirect("https://murty.au/luca");
-      break;
-  }
-
   try {
     // Serve remaining valid requests from the static site directory
     await context.send({
