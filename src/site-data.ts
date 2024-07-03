279e97515e42ad64b0011e12e5ed5b7a4414e394
Brendan Murty <brendan@murty.au>
2024-07-03 22:28:41 +1000

    Posts: Remove latest posts, show full list on home page

--- src/site-data.ts
+++ src/site-data.ts
@@ -1,18 +1,11 @@
 import { json2yaml } from "json2yaml/mod.ts";
 
-import { PostsLatest } from "./posts-latest.ts";
 import { LastFmLovedTracks } from "./lastfm-loved-tracks.ts";
 
 // Convert the contents of ".env" in to YAML format and save it as "build/_data/site.yml".
 // This allows for the Nunjucks templates to access these variables like this: {{ site.GOOGLE_ANALYTICS_SITE_CODE }}
 const EnvFileContent = Deno.readTextFileSync("./.env");
 let YmlFileContent = EnvFileContent.replaceAll("=", ": ");
-
-// Add the latest post properties to the site data
-const LatestPost = await PostsLatest();
-const DataLatestPost =
-  '{ "BLOG_POSTS_LATEST": ' + JSON.stringify(LatestPost) + "}";
-YmlFileContent += "\r" + json2yaml(DataLatestPost);
 
 // Add the Last.fm loved tracks to the site data
 const LastfmLovedTracks = await LastFmLovedTracks();
