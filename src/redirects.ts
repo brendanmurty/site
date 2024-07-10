8ac05d8d4c10b8f79c19c2860fb9968640fc2bde
Brendan Murty <brendan@murty.au>
2024-07-11 09:57:35 +1000

    Core: Progress on dynamic redirect page generation

--- src/redirects.ts
+++ src/redirects.ts
@@ -2,6 +2,8 @@ const RedirectTemplate = "./assets/redirect.html";
 const RedirectConfig = "./assets/redirects.json";
 const OutputDirectoryPrefix = "./docs/";
 
+// TODO: Consider using this instead of custom scripting: https://lume.land/plugins/redirects/
+
 // TODO: Deno.readFile RedirectConfig and loop through each item
 // TODO: Parse this and construct the from and to URLs
 // TODO: Deno.readFile RedirectTemplate and replace "https://murty.au" with "https://murty.au/TO_URL"
