8cf28d1574e14f0bb6feeea505017832dfd55da1
Brendan Murty <brendan@murty.au>
2024-08-23 15:39:18 +1000

    Core: Fix build of JSON Feed

--- scripts/build.sh
+++ scripts/build.sh
@@ -88,4 +88,5 @@ cp "$BUILD_DIR/_assets/css/styles.min.css" "$PUBLIC_DIR/css/styles.min.css"
 
 echo -e "${yellow}Building the JSON Feed for Brendan's posts${end}"
 
+mkdir -p $PUBLIC_DIR/brendan
 deno run -A --allow-read --allow-write --allow-env src/json-feed.ts
