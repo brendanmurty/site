0adb6fc78a58b67565e83387f3cef14a78c92b5f
Brendan Murty <brendan@murty.au>
2024-09-15 05:08:11 +0000

    DevOps: Simplify local web server

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,7 @@
-import { Application } from "@oak/oak/application";
+import { serveDir } from "@std/http/file-server";
 
-const app = new Application();
-
-app.use(async (context) => {
-  try {
-    // Serve remaining valid requests from the static site directory
-    await context.send({
-      root: Deno.cwd() + "/public",
-      index: "index.html"
-    });
-  } catch (error) {
-    // Catch any errors and simplify the log message for them
-    if (context.request?.url.href) {
-      console.log("Error: " + error.message + " (" + context.request.url.href + ")");
-    } else {
-      console.log("Error: " + error.message);
-    }
-
-    // Redirect to the home page
-    context.response.redirect("/");
-  }
+Deno.serve((request: Request) => {
+  return serveDir(request, {
+    fsRoot: "public"
+  });
 });
-
-// Listen for visitor requests on port 8000
-await app.listen({ port: 8000 });
