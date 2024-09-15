0bf6a9c1175aed94c6b9ba1739f6ac0522ae1bcc
Brendan Murty <brendan@murty.au>
2024-09-15 05:15:59 +0000

    DevOps: Improve local web server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { serveDir } from "@std/http/file-server";
 
-Deno.serve((request: Request) => {
+console.log('Serving "public" directory at http://localhost:8000');
+
+Deno.serve({ port: 8000 }, (request: Request) => {
   return serveDir(request, {
     fsRoot: "public"
   });
