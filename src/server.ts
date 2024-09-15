5760030961f0600b36c02061bf3a98a9327b0e0e
Brendan Murty <brendan@murty.au>
2024-09-15 16:21:02 +1000

    DevOps: Update local web server to make status message clearer

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { serveDir } from "@std/http/file-server";
 
-console.log('Serving "public" directory at http://localhost:8000');
+console.log('%cServing "public" directory at http://localhost:8000', "color: green");
 
 Deno.serve({ port: 8000 }, (request: Request) => {
   return serveDir(request, {
