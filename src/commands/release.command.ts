25734c2d77ed61a5fc097c0a4d1747bc31bac8fc
Brendan Murty <brendan@murty.au>
2024-09-17 04:59:37 +0000

    DevOps: Improve new release command

--- src/commands/release.command.ts
+++ src/commands/release.command.ts
@@ -18,7 +18,7 @@ if (!userConfirmed) {
 
 // Run tests
 
-console.log("%Running tests", "color: blue");
+console.log("%cRunning tests", "color: blue");
 
 const commandTest = new Deno.Command(Deno.execPath(), {
   args: ["run", "test"]
@@ -34,8 +34,8 @@ if (code !== 0) {
 
 console.log("%cTagging commit and pushing changes...", "color: blue");
 
-new Deno.Command("git", { args: ["tag", nextVersion] });
-new Deno.Command("git", { args: ["push", "--quiet"] });
-new Deno.Command("git", { args: ["push", "--tags", "--quiet"] });
+new Deno.Command("git", { args: ["--git-dir=" + Deno.cwd(), "tag", nextVersion] });
+new Deno.Command("git", { args: ["--git-dir=" + Deno.cwd(), "push", "--quiet"] });
+new Deno.Command("git", { args: ["--git-dir=" + Deno.cwd(), "push", "--tags", "--quiet"] });
 
 console.log("%cDone. A new GitHub Actions workflow should now remotely test and deploy these changes.", "color: green");
