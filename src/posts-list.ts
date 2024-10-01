41a18c1c1f695572d7afeed0e9cd43dd3b06909e
Brendan Murty <brendan@murty.au>
2024-10-01 15:02:13 +1000

    Core: Improve Post List logic

--- src/posts-list.ts
+++ src/posts-list.ts
@@ -15,19 +15,20 @@ export async function PostsList(
   for await (const item of Deno.readDir(postsDirectoryAbsolute)) {
     // Check this item is a valid file
     if (
-      item.isFile && item.name != "index.md" && item.name.slice(-3) == ".md"
+      item.isFile && item.name !== "index.md" && item.name.endsWith(".md")
     ) {
       // Extract general information from this file's name
       const postNoExtension: string = item.name.slice(0, -3);
-      const postUrl: string = postsUrl + postNoExtension + "/";
-      const postDate: string = item.name.slice(0, 4) + "-" +
-        item.name.slice(4, 6) + "-" + item.name.slice(6, 8) + "T09:00:00.000Z";
+      const postUrl: string = `${postsUrl}${postNoExtension}/`;
+      const postDate: string = `${item.name.slice(0, 4)}-${item.name.slice(4, 6)}-${
+        item.name.slice(6, 8)
+      }T09:00:00.000Z`;
 
       // Set a default title for this post
       let postTitle: string = postsDefaultTitle;
 
       // Attempt to extract the Post Title from the Markdown file's Frontmatter
-      const postContent = Deno.readTextFileSync(
+      const postContent = await Deno.readTextFile(
         join(postsDirectoryAbsolute, item.name),
       );
 
@@ -39,7 +40,7 @@ export async function PostsList(
             | YamlData
             | undefined;
 
-          if (frontmatterData) {
+          if (frontmatterData && frontmatterData.title) {
             postTitle = frontmatterData.title as string;
           }
         }
@@ -47,21 +48,18 @@ export async function PostsList(
 
       // Construct this post's item array and ensure it meets the requirements
       // of the JsonFeedItem type so it can also be used in the JSON feed
-      const postItem: JsonFeedItem = {
+      postItems.push({
         id: postUrl,
         url: postUrl,
         title: postTitle,
         date_published: postDate,
-        content_text: "Post by Brendan Murty, read more at " + postUrl,
-      };
-
-      // Add this post's information to the return array
-      postItems.push(postItem);
-
-      // Order the items in the return array by newest to oldest
-      postItems.sort((a, b) => -a.date_published.localeCompare(b.date_published));
+        content_text: `Post by Brendan Murty, read more at ${postUrl}`,
+      });
     }
   }
+
+  // Order the items in the return array by newest to oldest
+  postItems.sort((a, b) => b.date_published.localeCompare(a.date_published));
 
   return postItems;
 }
