b18807570fcd0548bf76f402b31c132679f67ba6
Brendan Murty <brendan@murty.au>
2024-11-08 16:38:47 +1100

    Core: Change interfaces to types as they're a better fit

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 // Define the accepted content in the Frontmatter of Post Markdown files
 //  - Adapted from https://github.com/lumeland/lume/blob/v1.0.0/types.ts
-export interface YamlData {
+export type YamlData = {
   tags?: string[];
   url?: string;
   draft?: boolean;
@@ -9,10 +9,10 @@ export interface YamlData {
   layout?: string;
   templateEngine?: string | string[];
   [index: string]: unknown;
-}
+};
 
 // Define the accepted content for JSON Feeds
-export interface JsonFeedData {
+export type JsonFeedData = {
   version: string;
   title: string;
   home_page_url?: string;
@@ -21,19 +21,19 @@ export interface JsonFeedData {
   author?: JsonFeedAuthor;
   language?: string;
   items: JsonFeedItem[];
-}
+};
 
 // Define the accepted content for JSON Feed Authors
-export interface JsonFeedAuthor {
+export type JsonFeedAuthor = {
   name: string;
   url: string;
-}
+};
 
 // Define the accepted content for JSON Feed Items
-export interface JsonFeedItem {
+export type JsonFeedItem = {
   id: string;
   url: string;
   title: string;
   date_published: string;
   content_text: string;
-}
+};
